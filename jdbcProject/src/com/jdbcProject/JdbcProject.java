783f5605977bbe001126ef9fb1e70dcc20dc8e47
harsha-pt4179 <harsha-pt4179@harsha-pt4179.csez.zohocorpin.com>
2021-06-30 14:57:31 +0530

    First Commit

---
+++ jdbcProject/src/com/jdbcProject/JdbcProject.java
@@ -0,0 +1,23 @@
+package com.jdbcProject;
+
+import java.sql.*;
+
+public class JdbcProject {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:5910/ticket","root","root3");
+		Statement st = con.createStatement();
+		String query = "SELECT * FROM USER";
+		ResultSet rs = st.executeQuery(query);
+		String loginName="";
+		while(rs.next())
+		{ 
+			loginName = rs.getString("loginname");
+			System.out.println(loginName);
+		}
+
+	}
+
+}
